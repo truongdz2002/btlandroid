1bf8e128a2c4884b4adf4e8e63f3725013d5c3f3
Phan Cong Long <pcl2k2@gmail.com>
2022-03-13 17:26:38 +0700

    Add Validate for RegisterActivity

--- app/src/main/java/com/example/appbanmaytinh/Package/RegisterActivity.java
+++ app/src/main/java/com/example/appbanmaytinh/Package/RegisterActivity.java
@@ -2,16 +2,23 @@ package com.example.appbanmaytinh.Package;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.appbanmaytinh.MainActivity;
 import com.example.appbanmaytinh.R;
 
 public class RegisterActivity extends AppCompatActivity {
-    Button register;
-    EditText gmail , password , rePassword;
+    Button btnRegister;
+    EditText gmail, password, rePassword;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +26,83 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         queryItem();
 
-        register.setOnClickListener(new View.OnClickListener() {
+        gmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+
+                if (gmail.getText().toString().trim().matches(emailPattern) && password.getText().toString().trim().equals(rePassword.getText().toString().trim()) && !password.getText().toString().trim().equals("") && !rePassword.getText().toString().trim().equals("")) {
+                    btnRegister.setEnabled(true);
+                } else {
+                    btnRegister.setEnabled(false);
+                }
+
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (gmail.getText().toString().trim().matches(emailPattern) && password.getText().toString().trim().equals(rePassword.getText().toString().trim()) && !password.getText().toString().trim().equals("") && !rePassword.getText().toString().trim().equals("")) {
+                    btnRegister.setEnabled(true);
+                } else {
+                    btnRegister.setEnabled(false);
+                }
+            }
+        });
+
+        rePassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (gmail.getText().toString().trim().matches(emailPattern) && password.getText().toString().trim().equals(rePassword.getText().toString().trim()) && !password.getText().toString().trim().equals("") && !rePassword.getText().toString().trim().equals("")) {
+                    btnRegister.setEnabled(true);
+                } else {
+                    btnRegister.setEnabled(false);
+                }
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    private  void queryItem(){
-        register = findViewById(R.id.btnRegisterRegister);
+    private void queryItem() {
+        btnRegister = findViewById(R.id.btnRegisterRegister);
         gmail = findViewById(R.id.edGmailRegister);
         password = findViewById(R.id.edPasswordRegister);
         rePassword = findViewById(R.id.edRePasswordRegigter);
